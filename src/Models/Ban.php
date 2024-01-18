4f62907bae081034fbaea2866ec551c7337a0c09
Christopher Skene <chris@xtfer.com>
2024-01-18 13:59:11 +1100

    Update Ban.php

--- src/Models/Ban.php
+++ src/Models/Ban.php
@@ -5,6 +5,7 @@ namespace Mchev\Banhammer\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
