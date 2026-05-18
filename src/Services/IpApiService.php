ca14a365e6e1046dc477ab31a708fda316298da5
Christopher Skene <chris.skene@gmail.com>
2026-05-18 18:32:17 +1000

    chore: inline var

--- src/Services/IpApiService.php
+++ src/Services/IpApiService.php
@@ -18,13 +18,11 @@ class IpApiService
 
         try {
             // Try to retrieve geolocation data from cache
-            $geolocationData = Cache::remember($cacheKey, now()->addDay(), function () use ($ip) {
-                $response = Http::get("http://ip-api.com/json/{$ip}?fields=status,message,countryCode,query");
+            return Cache::remember($cacheKey, now()->addDay(), function () use ($ip) {
+                $response = Http::get("https://ip-api.com/json/{$ip}?fields=status,message,countryCode,query");
 
                 return $response->json();
             });
-
-            return $geolocationData;
         } catch (\Exception $e) {
             // Log the error
             Log::error('IP-API Service Error: '.$e->getMessage(), ['exception' => $e]);
